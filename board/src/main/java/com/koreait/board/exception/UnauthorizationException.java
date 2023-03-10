a9f4d787300fb80f45add4d1527823f99ec64dd8
YuHoSeong <ghtjd026@gmail.com>
2023-03-10 17:41:38 +0900

    feat(security): add security config & jwtauthfilter

---
+++ board/src/main/java/com/koreait/board/exception/UnauthorizationException.java
@@ -0,0 +1,11 @@
+package com.koreait.board.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED,reason = "Need Authorization")
+public class UnauthorizationException extends RuntimeException{
+    public UnauthorizationException(){
+        super("Authentication is Required");
+    }
+}
