de5eb583f243b43cbdc6267f7736bbf5b063ef6f
YuHoSeong <ghtjd026@gmail.com>
2023-03-13 17:46:04 +0900

    feat: add  SecurityConfig, tokenProvider

---
+++ spring/src/main/java/com/spring/spring/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.spring.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apis")
+public class MainController {
+    
+    @GetMapping("/")
+    public String Main(){
+        return "Hello";
+    }
+}
---
+++ spring/src/main/java/com/spring/spring/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.spring.spring.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.spring.spring.provider.TokenProvider;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        try {
+            String token = parseToken(request);
+
+            if(token !=null && !token.equalsIgnoreCase("null")){
+                String email = tokenProvider.validate(token);
+
+                AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                securityContext.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(securityContext);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+    }
+
+    private String parseToken(HttpServletRequest request){
+        String Token = request.getHeader("Authorization");
+
+        boolean hasToken = StringUtils.hasText(Token);
+        if(!hasToken) return null;
+        boolean isBearer = Token.startsWith("Bearer ");
+        if(!isBearer) return null;
+
+        String jwt = Token.substring(7);
+        return jwt;
+    }
+    
+    
+}
